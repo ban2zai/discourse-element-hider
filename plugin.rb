fbb9c679948f819897fb8c27f73374004a0db1e2
ban2zai <aleskerov.02@gmail.com>
2026-04-02 17:04:29 +0700

    plugin rb (убрал лишнее)

--- plugin.rb
+++ plugin.rb
@@ -4,9 +4,3 @@
 # authors: calayx
 # url: https://github.com/ban2zai/discourse-element-hider
 
-add_admin_route "element_hider.admin_title", "element-hider"
-
-Discourse::Application.routes.append do
-  get "/admin/plugins/element-hider" => "admin/plugins#index",
-      constraints: StaffConstraint.new
-end
