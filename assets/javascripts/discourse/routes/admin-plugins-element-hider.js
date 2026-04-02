ab51be746c6157bb42aff30888d98b349146ec70
ban2zai <aleskerov.02@gmail.com>
2026-04-02 17:04:06 +0700

    Убрал admin route

--- assets/javascripts/discourse/routes/admin-plugins-element-hider.js
+++
@@ -1,9 +0,0 @@
-import DiscourseRoute from "discourse/routes/discourse";
-
-export default class AdminPluginsElementHiderRoute extends DiscourseRoute {
-  beforeModel() {
-    this.router.transitionTo("adminSiteSettings", {
-      queryParams: { filter: "element_hider" },
-    });
-  }
-}
